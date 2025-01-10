b657dccd2dd9debef9b666ed6c84d1b041e245fc
Tim C <conte.tim@gmail.com>
2025-01-10 00:20:30 +0000

    challenge completed

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,13 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        String result = "";
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                result += "*";
+            }
+            result += "\n";
+        }
+        return result;
     }
 }
